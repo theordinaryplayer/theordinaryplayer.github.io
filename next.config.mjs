5f62bbfd9935b12b1a9854aa20b02a0df2941724
Rosemary1337 <rayhanmahardika576@gmail.com>
2025-10-25 04:59:41 +0000

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,10 @@
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
+  trailingSlash: true,
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   basePath: "",
-  trailingSlash: true,
 };
 
 export default nextConfig;
